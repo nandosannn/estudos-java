3775c02964f284b01467a4218256c2e1377411a0
nandosannn <silvafernandosan@gmail.com>
2024-09-19 17:53:11 -0300

    issue_jpa

---
+++ módulo_22_jpa/issue_01_nivelamento/src/main/java/application/App.java
@@ -0,0 +1,18 @@
+package application;
+
+import dominio.Pessoa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+    }
+}
