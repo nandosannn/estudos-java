5652db4f4655150a01e1e862d7324ac7b2040bd2
nandosannn <silvafernandosan@gmail.com>
2024-09-21 09:15:02 -0300

    Update_modulo_jpa

--- módulo_22_jpa/issue_01_nivelamento/src/main/java/application/App.java
+++ módulo_22_jpa/issue_01_nivelamento/src/main/java/application/App.java
@@ -1,5 +1,10 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.swing.text.html.parser.Entity;
+
 import dominio.Pessoa;
 
 /**
@@ -8,11 +13,42 @@ import dominio.Pessoa;
  */
 public class App {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+        
+        /* //Inserir Dados no Banco de Dados
+        em.getTransaction().begin();
+
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit();
+
         System.out.println(p1);
         System.out.println(p2);
-        System.out.println(p3);
+        System.out.println(p3); */
+
+        /* // Procurar um dado na tabla e converter para objeto
+        Pessoa p = em.find(Pessoa.class, 2);
+
+        System.out.println(p);
+        */
+
+        //Remover elemento do banco de dados
+        // Objeto monitorado: é o objeto que acabou de ser inserido ou encontrado
+        Pessoa p = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        // A função remove só deleta do banco de dados um objeto monitorado
+        em.remove(p);
+        em.getTransaction().commit();
+
+        System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 }
--- módulo_22_jpa/issue_01_nivelamento/src/main/java/dominio/Pessoa.java
+++ módulo_22_jpa/issue_01_nivelamento/src/main/java/dominio/Pessoa.java
@@ -1,20 +1,30 @@
 package dominio;
 
-public class Pessoa {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
-    
-    public Pessoa(){
+
+    public Pessoa() {
 
     }
-    
+
     public Pessoa(Integer id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
-
 
     public Integer getId() {
         return id;
@@ -44,4 +54,4 @@ public class Pessoa {
     public String toString() {
         return "Pessoa [id=" + id + ", name=" + name + ", email=" + email + "]";
     }
-}   
+}
